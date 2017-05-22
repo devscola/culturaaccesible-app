0d07fdc3a1ba816c29dd1c19e3cf3da46a50a49e
Manel Romero <manel@manelromero.com>
2017-05-22 11:21:58 +0200

    Change some format

--- src/pages/exhibition-detail/exhibition-detail.ts
+++ src/pages/exhibition-detail/exhibition-detail.ts
@@ -4,14 +4,14 @@ import { ExhibitionsProvider } from '../../providers/exhibitions/exhibitions'
 
 @IonicPage()
 @Component({
-  selector: 'page-exhibition-detail',
-  templateUrl: 'exhibition-detail.html',
+    selector: 'page-exhibition-detail',
+    templateUrl: 'exhibition-detail.html',
 })
 export class ExhibitionDetail {
-  exhibition;
+    exhibition;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private service: ExhibitionsProvider) {
-    this.exhibition = navParams.get("exhibition")
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private service: ExhibitionsProvider) {
+        this.exhibition = navParams.get("exhibition")
+    }
 
 }
--- src/pages/exhibition-list/exhibition-list.ts
+++ src/pages/exhibition-list/exhibition-list.ts
@@ -3,35 +3,34 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ExhibitionsProvider } from '../../providers/exhibitions/exhibitions';
 import { ExhibitionDetail } from '../exhibition-detail/exhibition-detail'
 
-
 @IonicPage()
 @Component({
-  selector: 'page-exhibition-list',
-  templateUrl: 'exhibition-list.html',
+    selector: 'page-exhibition-list',
+    templateUrl: 'exhibition-list.html',
 })
 export class ExhibitionList {
-  exhibitions: Array<Object>
-  allExhibitions: Array<Object>
+    exhibitions: Array<Object>
+    allExhibitions: Array<Object>
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private service: ExhibitionsProvider) {
-    this.service.retrieveList().subscribe(exhibitions => {
-        this.allExhibitions = exhibitions
-        this.filterExhibitions()
-    })
-  }
-
-  filterExhibitions() {
-    let activeExhibitions: Array<Object> = []
-
-    for (let exhibition of this.allExhibitions) {
-        if (exhibition['show']) {
-            activeExhibitions.push(exhibition)
-        }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private service: ExhibitionsProvider) {
+        this.service.retrieveList().subscribe(exhibitions => {
+            this.allExhibitions = exhibitions
+            this.filterExhibitions()
+        })
     }
-    this.exhibitions = activeExhibitions
-  }
 
-  goToDetail(exhibition) {
-    this.navCtrl.push(ExhibitionDetail, {exhibition: exhibition})
-  }
+    filterExhibitions() {
+        let activeExhibitions: Array<Object> = []
+
+        for (let exhibition of this.allExhibitions) {
+            if (exhibition['show']) {
+                activeExhibitions.push(exhibition)
+            }
+        }
+        this.exhibitions = activeExhibitions
+    }
+
+    goToDetail(exhibition) {
+        this.navCtrl.push(ExhibitionDetail, {exhibition: exhibition})
+    }
 }
