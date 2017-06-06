17e1cd9d7ce882b4fc5d7c90672976619feef405
miguelonga <miguelmundiaragones@gmail.com>
2017-06-06 11:46:23 +0200

    showing map link by platform

--- src/pages/museum-detail/museum-detail.ts
+++ src/pages/museum-detail/museum-detail.ts
@@ -19,7 +19,7 @@ export class MuseumDetail {
     this.museumProvider.retrieveList().subscribe(list => {
       this.museum = list[0]
       this.hasLocation = list[0].link
-      this.coordinates = list[0].link.split('@')[1].split(',')[0] + list[0].link.split('@')[1].split(',')[1]
+      this.coordinates = list[0].link.split('@')[1].split(',')[0] + ',' + list[0].link.split('@')[1].split(',')[1]
       this.iosMapLink = "http://maps.apple.com/?q=" + this.coordinates
     })
   }
