867be70a3eb0a4e35d4ab69d2accaade591f2c84
miguelonga <miguelmundiaragones@gmail.com>
2017-07-24 14:00:55 +0200

    item detail plays video from s3

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @IonicPage()
 @Component({
@@ -11,17 +10,36 @@ export class ItemDetail {
 
   item = {
     "name": "Artwork Name",
-    "mediaFile": "https://www.youtube.com/embed/UQu6SWYR-e0",
+    "mediaFile": "https://s3.amazonaws.com/pruebas-cova/3minutes.mp4",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   }
 
-  file;
+  video;
+  action = 'play';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private domSanitizer: DomSanitizer) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    this.file = this.domSanitizer.bypassSecurityTrustResourceUrl(this.item.mediaFile)
+    this.video = document.getElementById("video");
+  }
+
+  play() {
+    this.video.play()
+    this.action = 'pause'
+  }
+
+  pause() {
+    this.video.pause()
+    this.action = 'play'
+  }
+
+  togglePlay() {
+    if(this.video.paused === true){
+      this.play()
+    } else {
+      this.pause()
+    }
   }
 
 }
