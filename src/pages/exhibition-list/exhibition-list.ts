6a11c8ea404f16ee0dae93095d922dacdd4a84d5
miguelonga <miguelmundiaragones@gmail.com>
2017-09-11 13:25:59 +0200

    modal after clicking an exhibition in exhibition list for asking which lenguaje the user wants

--- src/pages/exhibition-list/exhibition-list.ts
+++ src/pages/exhibition-list/exhibition-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, AlertController, NavParams } from 'ionic-angular';
 import { ExhibitionsProvider } from '../../providers/exhibitions/exhibitions';
 
 @IonicPage()
@@ -12,7 +12,10 @@ export class ExhibitionList {
     allExhibitions: Array<Object>
     hasExhibitions: boolean
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private service: ExhibitionsProvider) {
+    constructor(public navCtrl: NavController,
+                public alertCtrl: AlertController,
+                public navParams: NavParams,
+                private service: ExhibitionsProvider) {
         this.service.retrieveList().subscribe(exhibitions => {
             if(exhibitions.length > 0){
                 this.hasExhibitions = true
@@ -39,7 +42,46 @@ export class ExhibitionList {
         this.exhibitions = activeExhibitions
     }
 
+    isDownloaded(exhibition) {
+      return false
+    }
+
+    askLenguaje(exhibition) {
+      exhibition.lenguajes = ['es', 'cat', 'en']
+      let alert = this.alertCtrl.create({
+        title: 'Choose lenguaje',
+        message: 'in which lenguaje do you want to listen',
+        buttons: [
+          {
+            text: 'No',
+            role: 'cancel',
+            handler: () => {
+              console.log('Cancel clicked');
+              }
+            },
+            {
+            text: 'Yes',
+            handler: () => {
+              this.download(exhibition)
+            }
+          }
+        ]
+      });
+      exhibition.lenguajes.forEach((locale) => {
+        alert.addInput({type: 'radio', label: locale, value: locale})
+      })
+      alert.present();
+    }
+
+    download(exhibition) {
+      console.log('starting download')
+    }
+
     goToDetail(exhibition) {
-        this.navCtrl.push('ExhibitionDetail', {exhibition: exhibition})
+        if(this.isDownloaded(exhibition)){
+            this.navCtrl.push('ExhibitionDetail', {exhibition: exhibition})
+        }else{
+            this.askLenguaje(exhibition)
+        }
     }
 }
